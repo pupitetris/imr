a4e3cdacdcac3165dcbced2e48f65146fa9f6314
Arturo Espinosa <arturoea@gmail.com>
2012-07-30 18:28:27 -0500

    Stubs for the database enumerations.

--- sql/03-types.sql
+++ sql/03-types.sql
@@ -1,1 +1,40 @@
 -- Application-specific data types.
+
+CREATE TYPE imr_msg_code AS ENUM (
+);
+
+CREATE TYPE imr_msg_type AS ENUM (
+);
+
+CREATE TYPE imr_msg_area AS ENUM (
+);
+
+CREATE TYPE imr_lang AS ENUM (
+);
+
+CREATE TYPE imr_record_status AS ENUM (
+);
+
+CREATE TYPE imr_asenta_type AS ENUM (
+);
+
+CREATE TYPE imr_product_type AS ENUM (
+);
+
+CREATE TYPE imr_persona_type AS ENUM (
+);
+
+CREATE TYPE imr_gender AS ENUM (
+);
+
+CREATE TYPE imr_phone_type AS ENUM (
+);
+
+CREATE TYPE imr_level AS ENUM (
+);
+
+CREATE TYPE imr_teleheal_type AS ENUM (
+);
+
+CREATE TYPE imr_remedy_type AS ENUM (
+);
