0c38f3b7097b7abbd01b9d9b1680e1f4a2faadcc
Arturo Espinosa <arturoea@gmail.com>
2012-08-16 01:30:19 -0500

    Use the new macro. And, a new rp that fetches the users list.

--- sql/05-functions.sql
+++ sql/05-functions.sql
@@ -1,15 +1,24 @@
 -- Application-specific functions.
 
-CREATE OR REPLACE FUNCTION rp_user_get_type(_uid charp_user_id)
-  RETURNS imr_account_type AS
-$BODY$
+M4_FUNCTION( rp_user_get_type(_uid charp_user_id), imr_account_type, 
+			 STABLE, M4_DEFN(user), 'Get the user level for UI configuration purposes.', «
 DECLARE
 		_res imr_account_type;
 BEGIN
 		SELECT account_type INTO _res FROM account WHERE persona_id = _uid;
 		RETURN _res;
-END
-$BODY$
-  LANGUAGE plpgsql STABLE;
-ALTER FUNCTION rp_user_get_type(_uid charp_user_id) OWNER TO M4_DEFN(user);
-COMMENT ON FUNCTION rp_user_get_type(_uid charp_user_id) IS 'Get the user level for UI configuration purposes.';
+END »);
+
+
+M4_FUNCTION( rp_user_list_get(_uid charp_user_id),
+			 «TABLE( persona_id integer, username varchar, prefix varchar, name varchar, 
+			 		 paterno varchar, materno varchar, status charp_account_status )»,
+			 STABLE, M4_DEFN(user), 'Get the list of all users for the user''s instance.', «
+BEGIN
+		RETURN QUERY SELECT a.persona_id, a.username, p.prefix, p.name, p.paterno, p.materno, a.status 
+			   		 		FROM account AS a1 
+			   		 			 JOIN account AS a USING (inst_id) 
+								 JOIN persona AS p ON (a.persona_id = p.persona_id) 
+							WHERE a1.persona_id = _uid;
+END »);
+
