36cabc1dfd0ea380fe53c5f34b3d9cf79d6e39af
Arturo Espinosa <arturoea@gmail.com>
2012-08-17 13:04:25 -0500

    Added another test user.

--- sql/99-test.sql
+++ sql/99-test.sql
@@ -5,10 +5,17 @@ BEGIN TRANSACTION;
 	  		 VALUES (DEFAULT, 1, 'test', 'ACTIVE');
 
 	  INSERT INTO persona(persona_id, inst_id, type, prefix, name, paterno, materno, gender, picture, remarks, status)
-	  		 VALUES (DEFAULT, 1, 'INST', NULL, 'Test', NULL, NULL, 'MALE', NULL, NULL, 'ACTIVE');
+	  		 VALUES (DEFAULT, 1, 'INST', NULL, 'Test', NULL, NULL, 'FEMALE', NULL, NULL, 'ACTIVE');
 
       -- password is ``blah''
       INSERT INTO account(persona_id, inst_id, username, passwd, account_type, status)
       	     VALUES (1, 1, 'testuser', '6f1ed002ab5595859014ebf0951522d9', 'ADMIN', 'ACTIVE');
+
+	  INSERT INTO persona(persona_id, inst_id, type, prefix, name, paterno, materno, gender, picture, remarks, status)
+	  		 VALUES (DEFAULT, 1, 'USER', 'Mr.', 'Alfonso', 'Otero', NULL, 'MALE', NULL, NULL, 'ACTIVE');
+
+      -- password is ``blah''
+      INSERT INTO account(persona_id, inst_id, username, passwd, account_type, status)
+      	     VALUES (2, 1, 'tito', 'd0655af3824a90cf215bedc890a9028a', 'SUPERUSER', 'DISABLED');
 
 COMMIT TRANSACTION;
