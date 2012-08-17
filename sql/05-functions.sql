a1a81c4948aa13f59f7355d667f27b3a2c0ffa14
Arturo Espinosa <arturoea@gmail.com>
2012-08-17 13:03:55 -0500

    New remote procedure for deleteng users. Hacky function that allows easy administration and checking of the current, simple per-level user permissions. Also, when listing users, check that they don't have the deleted flag.

--- sql/05-functions.sql
+++ sql/05-functions.sql
@@ -1,5 +1,16 @@
 -- Application-specific functions.
 
+
+M4_FUNCTION( «account_type_has_perm(_type imr_account_type, _perm imr_perm)», boolean,
+			 IMMUTABLE, M4_DEFN(user), 'Check if a given type of account can perform an action.', «
+DECLARE
+		_level varchar;
+BEGIN
+		_level := _type;
+		RETURN _perm::imr_perm = ANY (enum_range (null::imr_perm, _level::imr_perm));
+END »);
+
+
 M4_FUNCTION( rp_user_get_type(_uid charp_user_id), imr_account_type, 
 			 STABLE, M4_DEFN(user), 'Get the user level for UI configuration purposes.', «
 DECLARE
@@ -19,6 +30,31 @@ BEGIN
 			   		 		FROM account AS a1 
 			   		 			 JOIN account AS a USING (inst_id) 
 								 JOIN persona AS p ON (a.persona_id = p.persona_id) 
-							WHERE a1.persona_id = _uid;
+							WHERE a1.persona_id = _uid AND a.status <> 'DELETED';
 END »);
+
+
+M4_FUNCTION( «rp_user_remove(_uid charp_user_id, _persona_id integer)»,
+			 boolean, VOLATILE, M4_DEFN(user), 'Mark an user as DELETED.', «
+DECLARE
+		_my_type imr_account_type;
+		_user_type varchar;
+BEGIN
+		IF _uid = _persona_id THEN PERFORM charp_raise('USERPERM'); END IF;
+
+		SELECT a1.account_type, a2.account_type INTO _my_type, _user_type 
+			   FROM account AS a1 JOIN account AS a2 USING (inst_id)
+			   WHERE a1.persona_id = _uid AND a2.persona_id = _persona_id;
+
+		IF NOT FOUND THEN PERFORM charp_raise('NOTFOUND'); END IF;
+		IF NOT account_type_has_perm(_my_type, 'USER_DELETE') OR 
+		   NOT account_type_has_perm (_my_type, _user_type::imr_perm) THEN 
+		   PERFORM charp_raise('USERPERM');
+		END IF;
+
+		UPDATE account SET status='DELETED' WHERE persona_id = _persona_id;
+
+		RETURN TRUE;
+END »);
+		
 
