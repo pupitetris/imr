2c4459e0d2bb7412d122f48dcf45129d249508bb
Arturo Espinosa <arturoea@gmail.com>
2012-08-21 23:23:23 -0500

    Starting new widget for editing personal contact information.

---
+++ clients/IMR-patient/IMR-patient/PersonaEditor.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace IMRpatient
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PersonaEditor : Gtk.Bin
+	{
+		public PersonaEditor ()
+		{
+			this.Build ();
+		}
+	}
+}
+
