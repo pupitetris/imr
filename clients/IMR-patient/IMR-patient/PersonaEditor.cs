aa1023d9503501fc4b88d067032fc4f734dadcf6
Arturo Espinosa <arturoea@gmail.com>
2012-08-22 00:10:17 -0500

    Set the default image depending on the person's gender.

--- clients/IMR-patient/IMR-patient/PersonaEditor.cs
+++ clients/IMR-patient/IMR-patient/PersonaEditor.cs
@@ -5,9 +5,26 @@ namespace IMRpatient
 	[System.ComponentModel.ToolboxItem(true)]
 	public partial class PersonaEditor : Gtk.Bin
 	{
+		private bool imageSet = false;
+
 		public PersonaEditor ()
 		{
 			this.Build ();
+
+			Gdk.Color white = new Gdk.Color (255, 255, 255);
+			personaImgFrame.ModifyBg (Gtk.StateType.Prelight, white);
+			personaImg.ModifyBg (Gtk.StateType.Prelight, white);
+		}
+
+		protected void OnRadioMaleToggled (object sender, EventArgs e)
+		{
+			if (!imageSet) {
+				if (radioMale.Active) {
+					personaImg.Pixbuf = Gdk.Pixbuf.LoadFromResource ("IMRpatient.img.user_male.png");
+				} else {
+					personaImg.Pixbuf = Gdk.Pixbuf.LoadFromResource ("IMRpatient.img.user_female.png");
+				}
+			}
 		}
 	}
 }
