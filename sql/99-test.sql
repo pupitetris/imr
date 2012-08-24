6ad4441586cd46c990f28bc5858604dae2dbf09e
Arturo Espinosa <arturoea@gmail.com>
2012-08-24 03:14:26 -0500

    Another test user, an OPERATOR.

--- sql/99-test.sql
+++ sql/99-test.sql
@@ -18,4 +18,10 @@ BEGIN TRANSACTION;
       INSERT INTO account(persona_id, inst_id, username, passwd, account_type, status)
       	     VALUES (2, 1, 'tito', 'd0655af3824a90cf215bedc890a9028a', 'SUPERUSER', 'ACTIVE');
 
+	  INSERT INTO persona(persona_id, inst_id, type, prefix, name, paterno, materno, gender, picture, remarks, status)
+	  		 VALUES (DEFAULT, 1, 'USER', 'Dr.', 'Fulano', 'Sultano', NULL, 'MALE', NULL, NULL, 'ACTIVE');
+
+      INSERT INTO account(persona_id, inst_id, username, passwd, account_type, status)
+      	     VALUES (3, 1, 'fulano', 'd0655af3824a90cf215bedc890a9028a', 'OPERATOR', 'ACTIVE');
+
 COMMIT TRANSACTION;
