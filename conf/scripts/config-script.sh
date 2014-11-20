db413ebcb207930bfad269d89ca09db61e9bbe8c
Arturo Espinosa <arturoea@gmail.com>
2014-11-20 14:16:43 -0600

    * Upgraded conf scripts.

--- conf/scripts/config-script.sh
+++ conf/scripts/config-script.sh
@@ -1,55 +1,97 @@
 # Sourced by conf/config.sh
 
-CONF_DATABASE=${PREFIX}_PGDATABASE
-CONF_HOST=${PREFIX}_PGHOST
-CONF_PORT=${PREFIX}_PGPORT
-CONF_USER=${PREFIX}_PGUSER
-CONF_DIR=${PREFIX}DIR
+CONF_DATABASE=$(prefix DB_DATABASE)
+[ -z "$CONF_DATABASE" ] && CONF_DATABASE=$DB_DATABASE
+# If there was a command-line override (-db), use it.
+[ ! -z "$DB" ] && CONF_DATABASE=$DB
 
-export PGDATABASE=${!CONF_DATABASE}
-export PGHOST=${!CONF_HOST}
-export PGPORT=${!CONF_PORT}
-export PGUSER=${!CONF_USER}
+CONF_HOST=$(prefix DB_HOST)
+[ -z "$CONF_HOST" ] && CONF_HOST=$DB_HOST
 
-# Define <PREFIX>DIR in your bash_profile.
-if [ ! -d "${!CONF_DIR}" ]; then
-    echo Variable $CONF_DIR is not defined. >&2
-    exit 1
-fi
+CONF_PORT=$(prefix DB_PORT)
+[ -z "$CONF_PORT" ] && CONF_PORT=$DB_PORT
 
-BASEDIR="${!CONF_DIR}"
-CONFIGDIR="$BASEDIR"/conf
-TESTDIR="$BASEDIR"/scripts/test
+CONF_USER=$(prefix DB_USER)
+[ -z "$CONF_USER" ] && CONF_USER=$DB_USER
 
-# Directory where the project's SQL is found.
-SQLDIR="$BASEDIR/sql"
+BASEDIR=$CONF_DIR
+CONFIGDIR=$BASEDIR/conf
+DB_CONFIGDIR=$BASEDIR/$DB_TYPE/conf
+TESTDIR=$BASEDIR/scripts/test
 
-# Under Cygwin, transform the directory to Windows notation, 
-# since that's what the Windows-native Postgres requires for COPYs.
+OUR_RAND=$RANDOM
+
 if [ $(uname -o) = 'Cygwin' ]; then
     IS_CYGWIN=1
-    SQLDIR=$(sed 's#/cygdrive/\(\w\+\)/#\1:/#' <<< "$SQLDIR")
-    DB_LOCALE=$DB_LOCALE_WIN
 fi
 
-# This obscure function runs psql with our own set of configuration variables
-# and filters out unwanted psql NOTICEs.
-function psql_filter {
-    {
+# Directory where the project's SQL is found.
+SQLDIR=$BASEDIR/$DB_TYPE/sql
+
+if [ $DB_OS = "win" ]; then
+	# Transform the sql directory to Windows notation, 
+	# since that's what the Windows-native Postgres requires for COPYs.
+	WIN_SQLDIR=$(cygpath -aw "$SQLDIR")
+fi
+
+source "$DB_CONFIGDIR"/scripts/config-script.sh
+
+sqlvars_end=$CONFIGDIR/scripts/sqlvars_end.m4
+if [ ! -e "$sqlvars_end" ]; then
+	echo 'm4_changecom(«--», «
+»)
+m4_divert«»m4_dnl
+m4_undefine(' > "$sqlvars_end"
+	echo -n m4_dumpdef | m4 -P 2>&1 | grep -v '^\(m4_defn\|m4_dnl\|m4_patsubst\):' | sed 's/^\([^:]\+\).*/		«\1»,/g' >> $sqlvars_end
+	echo '		«CONF_USER»,
+		«CONF_DATABASE»,
+		«CONF_LOCALE»,
+		«CONF_COLLATE»,
+		«CONF_SQLDIR»,
+		«CONF_USER_PASSWD»,
+		«DEFINE»)«»m4_dnl' >> "$sqlvars_end"
+fi
+
+# Run the db client with our own set of configuration variables.
+function db_filter {
 	local sql_file=$1
-	echo $sql_file;
 	shift
-	m4 -P "$CONFIGDIR"/config_init.m4 \
-	    -D CONF_USER=${!CONF_USER} \
-	    -D CONF_DATABASE=${!CONF_DATABASE} \
-	    -D CONF_LOCALE="$DB_LOCALE" \
-	    -D CONF_SQLDIR="$SQLDIR" \
-	    "$CONFIGDIR"/config.m4 "$CONFIGDIR"/config_end.m4 "$sql_file" > ${sql_file}-tmp
-	psql -q -f ${sql_file}-tmp "$@" 2>&1 >&3 3>&- | grep -v ''\
-'NOTICE:  \(function\|funci.n\) [^)]\+) \(does not exist, skipping\|no existe\)\|'\
-'NOTICE:  CREATE TABLE / PRIMARY KEY \(will create implicit index\|crear. el .ndice impl.cito\)\|'\
-'NOTICE:  \(constraint\|no existe la restricci.n\)\|'\
-'NOTICE:  \(view\|la vista\)' >&2 3>&-
-	rm -f ${sql_file}-tmp
-    } 3>&1
+
+	local sqldir=$SQLDIR
+	if [ $DB_OS = "win" ]; then sqldir=$WIN_SQLDIR; fi
+
+	local tmp=${sql_file}-$OUR_RAND-tmp
+	local status=0
+
+	(
+		umask 0177
+		m4 -P "$CONFIGDIR"/scripts/sqlvars_init.m4 \
+			"$DB_CONFIGDIR"/scripts/sqlvars_init.m4 \
+			-D CONF_USER="$CONF_USER" \
+			-D CONF_USER_PASSWD="$CONF_USER_PASSWD" \
+			-D CONF_DATABASE="$CONF_DATABASE" \
+			-D CONF_LOCALE="$DB_LOCALE" \
+			-D CONF_COLLATE="$DB_COLLATE" \
+			-D CONF_SQLDIR="$sqldir" \
+			"$CONFIGDIR"/sqlvars.m4 "$CONFIGDIR"/scripts/sqlvars_end.m4 "$sql_file" > "$tmp"
+	) || status=4
+
+	if [ $status = 0 ]; then
+	    if [ -z "$DRY_RUN" ]; then
+		echo $sql_file
+		db_client "$tmp" "$@" || status=4
+	    else
+		echo '-- >>>' $sql_file '<<<'
+		local width=$(($(wc -l < "$tmp" | wc -c) - 1))
+		nl -ba -w $width -s : "$tmp"
+		echo
+	    fi
+	fi
+
+	rm -f "$tmp"
+	return $status
+}
+
+function db_filter_or_exit {
+    db_filter "$@" || exit $?
 }
--- conf/scripts/sqlvars_init.m4
+++ conf/scripts/sqlvars_init.m4
@@ -6,35 +6,5 @@ m4_define( «DEFINE»,
 		       «m4_errprint(m4___program__:m4___file__:m4___line__:«$0: 2 arguments required, got $#»)
 		        m4_m4exit(1))»)»)
 
-m4_define( «M4_CATALOG»,
-«
-\echo '$1'
-DELETE FROM $1;
-COPY $1 FROM 'M4_DEFN(sqldir)/catalogs/$1.csv' WITH (FORMAT csv, HEADER TRUE, DELIMITER '|', QUOTE '"')»)
-
-# M4_FUNCTION («prototype», «return type», function type {IMMUTABLE|STABLE|VOLATILE}, owner, 'comment', «body»)
-m4_define( «M4_FUNCTION»,
-«DROP FUNCTION IF EXISTS $1;
-CREATE FUNCTION $1
-  RETURNS $2 AS
-$BODY$
-$6
-$BODY$
-  LANGUAGE plpgsql $3;
-ALTER FUNCTION $1 OWNER TO $4;
-COMMENT ON FUNCTION $1 IS $5»)
-
-# M4_SQL_FUNCTION («prototype», «return type», function type {IMMUTABLE|STABLE|VOLATILE}, owner, 'comment', «body»)
-m4_define( «M4_SQL_FUNCTION»,
-«DROP FUNCTION IF EXISTS $1;
-CREATE FUNCTION $1
-  RETURNS $2 AS
-$BODY$
-$6
-$BODY$
-  LANGUAGE sql $3;
-ALTER FUNCTION $1 OWNER TO $4;
-COMMENT ON FUNCTION $1 IS $5»)
-
 m4_define( «M4_DEFN»,
 	   «m4_defn(««$»$1»)»)
