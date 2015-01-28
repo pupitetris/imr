7e5685b745e8ea950e65ebe57c4a8774a62fad97
Arturo Espinosa <arturoea@gmail.com>
2015-01-27 21:06:08 -0600

    New TriState Checkbox for nullable boolean value representations.
    Aesthtic changes to GUI (missing textbox frames in editors).

---
+++ clients/IMR-patient/IMR-patient/Widgets/CheckTristate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace IMRpatient
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class CheckTristate : Gtk.CheckButton
+	{
+		private bool IgnoreChange = false;
+
+		public CheckTristate ()
+		{
+			this.Build ();
+		}
+
+		public bool? TriState {
+			set {
+				if (value == null)
+					Inconsistent = true;
+				else {
+					Inconsistent = false;
+					if (value == true)
+						Active = true;
+					else
+						Active = false;
+				}
+			}
+
+			get {
+				if (Inconsistent)
+					return null;
+				return Active;
+			}
+		}
+
+		protected void OnTristateToggled (object sender, EventArgs e)
+		{
+			if (IgnoreChange)
+				return;
+
+			if (Inconsistent) {
+				Inconsistent = false;
+				IgnoreChange = true;
+				Active = true;
+				IgnoreChange = false;
+				return;
+			}
+
+			if (Active) {
+				Inconsistent = true;
+			}
+		}
+	}
+}
+
